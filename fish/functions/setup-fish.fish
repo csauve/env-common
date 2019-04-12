51af51f900753f41c9fcb2ec18060dcc5f49b476
csauve <csauve@t3hz0r.com>
2019-04-12 00:02:26 -0700

    add .npmrc config

--- fish/functions/setup-fish.fish
+++ fish/functions/setup-fish.fish
@@ -1,8 +1,7 @@
 function setup-fish
-  # common
   set -Ux EDITOR vim
   set -Ux LSCOLORS GxFxCxDxBxegedabagaced
-  set -U fish_user_paths ~/bin ~/.cargo/bin
+  set -U fish_user_paths ~/bin ~/.cargo/bin ~/.npm/bin
 
   echo "Done!"
 end

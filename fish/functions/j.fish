b7cfeb505fcc28364730961861c9dc3d49d660df
csauve <sauvec@abebooks.com>
2019-04-15 16:27:24 -0700

    update jump command

--- fish/functions/j.fish
+++ fish/functions/j.fish
@@ -1,6 +1,11 @@
 function j
   set term $argv[1]
   set result (grep -i $term $cwd_hist_file | sort | head -n 1)
-  cd $result
+  if test -d $result
+    cd $result
+  else
+    echo "Directory $result does not exist"
+    #grep -v $result $cwd_hist_file > $cwd_hist_file
+  end
   echo $result
 end

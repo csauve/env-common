077c0b6aeeb368f0a85629b3e42afcaad345c66e
csauve <csauve@t3hz0r.com>
2019-05-26 18:36:40 -0700

    Support directory signature in prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -7,11 +7,13 @@ end
 function fish_prompt
   cwd_hist
 
+  #set dir_sig (string join "" (exa -la | string sub -l 1 | sort | uniq -c) | string replace -a " " "")
   set pwd_base (basename (prompt_pwd))
   set pwd_dir (string split -r -m 1 $pwd_base (prompt_pwd))[1]
 
   print_c $c_secondary (whoami)"@"(hostname -s)" "
   #print_c $c_faded "(　ﾟ∀ﾟ)っ"
+  #print_c $c_faded [$dir_sig]
   print_c $c_faded $pwd_dir
   print_c $c_primary $pwd_base
   print_c normal (__fish_git_prompt)

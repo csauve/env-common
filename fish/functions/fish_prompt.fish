2f888051175327dd82d6960603d4e2dfe2225eca
csauve <sauvec@abebooks.com>
2019-07-31 10:23:07 -0700

    Set prompt based on ssh or root status

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -10,8 +10,11 @@ function fish_prompt
   set pwd_base (basename (prompt_pwd))
   set pwd_dir (string split -r -m 1 $pwd_base (prompt_pwd))[1]
 
-  print_c $c_secondary (whoami)"@"(hostname -s)" "
-  #print_c $c_faded "(　ﾟ∀ﾟ)っ"
+  if test -n "$SSH_CONNECTION" -o (whoami) = root
+    print_c $c_secondary (whoami)"@"(hostname -s)" "
+  else
+    print_c $c_secondary "(　ﾟ∀ﾟ)っ "
+  end
   print_c $c_faded $pwd_dir
   print_c $c_primary $pwd_base
   print_c normal (__fish_git_prompt)

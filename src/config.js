f324986f378ca8c583c6f7a861fb605e5b6a91b2
csauve <csauve@t3hz0r.com>
2019-08-04 01:46:28 -0700

    Switch to polybar

--- src/config.js
+++ src/config.js
@@ -1,17 +1,25 @@
+//https://stackoverflow.com/questions/15852122/hex-transparency-in-colors
 const c = {
+  transparent: "#00000000",
   black: "#0f111e",
+  black_transparent: "#e60f111e",
   dark_gray: "#111926",
   gray: "#263e48",
   light_gray: "#666666",
   red: "#c82438",
-  green_dark: "#9b1e2e",
   green: "#6bdba1",
   yellow: "#fdc198",
   blue: "#3da0bf",
   magenta: "#db86ba",
   cyan: "#62cdcd",
   white: "#bad2d6",
-  transparent: "#00000000"
+  width_small: 4,
+  width_normal: 12,
+  width_bar: 36 + 8,
+  font: "Mononoki",
+  font_icons: "FontAwesome",
+  font_size: 14,
+  dpi: 80
 };
 
 console.log(JSON.stringify({
@@ -19,9 +27,13 @@ console.log(JSON.stringify({
   content: {
     fg: c.white,
     bg: c.black,
+    bg_transparent: c.black_transparent,
+    bg_opacity: 0.90,
     faded: c.light_gray,
     good: c.green,
     bad: c.red,
+    fg_urent: c.yellow,
+    fg_focused: c.red,
     accent: c.magenta,
     accent_faded: c.light_gray,
   },
@@ -29,7 +41,7 @@ console.log(JSON.stringify({
     fg: c.white,
     bg: c.black,
     border: {
-      focused: c.green_dark,
+      focused: c.red,
       active: c.dark_gray,
       inactive: c.dark_gray,
       indicator: c.dark_gray,
@@ -38,7 +50,7 @@ console.log(JSON.stringify({
   },
   notice: {
     border: {
-      focused: c.green_dark,
+      focused: c.red,
       active: c.black,
       inactive: c.black,
       urgent: c.red,
@@ -50,7 +62,7 @@ console.log(JSON.stringify({
       urgent: c.black,
     },
     bg: {
-      focused: c.green_dark,
+      focused: c.red,
       active: c.black,
       inactive: c.black,
       urgent: c.red,
